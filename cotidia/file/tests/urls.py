a14e8e144dd1c8bdfb47259836a6f7af2bbdaa05
Guillaume Piot <guillaume@cotidia.com>
2017-09-14 15:42:07 +0100

    Make tests run with account and admin setup.

--- cotidia/file/tests/urls.py
+++ cotidia/file/tests/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 
+from cotidia.account.views.admin import dashboard
 from .views import PublicUpload
 
 urlpatterns = [
@@ -9,4 +10,13 @@ urlpatterns = [
         name="file-upload-public"),
     url(r'^admin/file/', include('cotidia.file.urls.admin.file',
         namespace="file-admin")),
+    url(r'^admin/account/', include('cotidia.account.urls.admin',
+        namespace="account-admin")),
+    url(r'^admin/mail/', include('cotidia.mail.urls',
+        namespace="mail-admin")),
+    url(
+        r'^admin/$',
+        dashboard,
+        name="dashboard"
+    ),
 ]
