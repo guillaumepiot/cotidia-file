e7ec65aa6bdf725e6214809bdaa8849f5a7cc17a
Guillaume Piot <guillaume@cotidia.com>
2017-09-14 15:42:31 +0100

    Add file detail and file list view.

--- cotidia/file/urls/admin/file.py
+++ cotidia/file/urls/admin/file.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from cotidia.file.views.admin.file import (
     FileList,
+    FileDetail,
     FileDelete
 )
 
@@ -11,6 +12,10 @@ urlpatterns = [
         r'^$',
         FileList.as_view(),
         name='file-list'),
+    url(
+        r'^(?P<pk>[\d]+)$',
+        FileDetail.as_view(),
+        name='file-detail'),
     url(
         r'^(?P<pk>[\d]+)/delete$',
         FileDelete.as_view(),
--- cotidia/file/views/admin/file.py
+++ cotidia/file/views/admin/file.py
@@ -1,4 +1,8 @@
-from cotidia.admin.views import AdminListView, AdminDeleteView
+from cotidia.admin.views import (
+    AdminListView,
+    AdminDetailView,
+    AdminDeleteView
+)
 
 from cotidia.file.models import File
 
@@ -9,6 +13,53 @@ class FileList(AdminListView):
         ('Date Created', 'created_at'),
     )
     model = File
+
+
+class FileDetail(AdminDetailView):
+    model = File
+    fieldsets = [
+        {
+            "legend": "File details",
+            "fields": [
+                [
+                    {
+                        "label": "Name",
+                        "field": "name",
+                    },
+                    {
+                        "label": "File path",
+                        "field": "f",
+                    },
+                ],
+                {
+                    "label": "Date Created",
+                    "field": "created_at",
+                },
+                {
+                    "label": "MIME type",
+                    "field": "mimetype",
+                }
+            ]
+        },
+        {
+            "legend": "Categorisation",
+            "fields": [
+                {
+                    "label": "Related to object",
+                    "field": "content_object",
+                },
+                {
+                    "label": "Taxonomy",
+                    "field": "taxonomy",
+                }
+            ]
+        },
+        {
+            "legend": "Preview",
+            "template_name": "admin/file/file/preview.html"
+        }
+
+    ]
 
 
 class FileDelete(AdminDeleteView):
