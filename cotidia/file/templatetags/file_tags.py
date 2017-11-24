b2dbe5b34cf964a796ac935bcde83878e233d806
Guillaume Piot <guillaume@cotidia.com>
2017-11-24 17:20:24 +0000

    Order by created at.

--- cotidia/file/templatetags/file_tags.py
+++ cotidia/file/templatetags/file_tags.py
@@ -10,7 +10,7 @@ def get_files_for_object(content_type_id, object_id):
     return File.objects.filter(
         content_type__id=content_type_id,
         object_id=object_id
-    ).order_by('-updated_at')
+    ).order_by('-created_at')
 
 
 @register.simple_tag
